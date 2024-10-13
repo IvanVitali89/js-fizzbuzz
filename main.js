b61f7f8b5faae66ac06c1e2a389910cb0d0474bd
IvanVitali89 <ertre89@gmail.com>
2024-10-13 11:31:40 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+//creare un ciclo if che conta fino a 100
+//se il contatore % 15 da 0 allora stampa FizzBuzz
+//se il contatore % 3 da 0 allora stampa Fizz
+//se il contatore % 5 da 0 allora stampa Buzz
+//altrimenti stampa il contatore
+
+
+let i=1
+for (i=1; i<=100; i++) {
+if ( i%15 ===0){
+    console.log('FizzBuzz')
+    } else if ( i%3===0){
+        console.log('Fizz')
+    }else if ( i%5===0){
+        console.log('Buzz')
+    } else
+    console.log(i);
+}
